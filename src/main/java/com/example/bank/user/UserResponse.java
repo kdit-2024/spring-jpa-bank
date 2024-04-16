ad86bc043e5d352f85059c149b56af21cfb63348
coderyu5523 <coderyu5523@naver.com>
2024-04-16 10:13:24 +0900

    user DTO 수정

--- src/main/java/com/example/bank/user/UserResponse.java
+++ src/main/java/com/example/bank/user/UserResponse.java
@@ -8,12 +8,10 @@ public class UserResponse {
     @Data
     public static class DTO{
         private String username ;
-        private String password;
         private String fullname;
 
         public DTO(User user) {
             this.username = user.getUsername();
-            this.password = user.getPassword();
             this.fullname = user.getFullname();
         }
     }
